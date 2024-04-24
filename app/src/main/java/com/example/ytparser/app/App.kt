94567dfbc71d544011ba6bcd6dfff00bc59b508d
fundroiddev <152708247+fundroiddev@users.noreply.github.com>
2024-04-24 21:54:40 +0500

    Delete app/src/main/java/com/example/ytparser/app directory

--- app/src/main/java/com/example/ytparser/app/App.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ytparser.app
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.android.qualifiers.ApplicationContext
-
-@HiltAndroidApp
-class App : Application()
